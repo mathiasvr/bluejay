51dc79f4d73d6b9eb83adc5e776b4e4f98c20420
Mathias Rasmussen <mathiasvr@gmail.com>
2020-11-08 02:04:03 +0100

    ci: Update commitlint config

--- .github/workflows/commitlint.config.js
+++ .github/workflows/commitlint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [ 2, 'always', ['sentence-case'] ],
-  }
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': [2, 'always', ['sentence-case']],
+		'body-leading-blank': [2, 'always'],
+		'footer-leading-blank': [2, 'always']
+	}
 };
